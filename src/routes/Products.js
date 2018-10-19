2f78ef0613c6ae51f3f4fdea97d55b51cd127081
SabithHaneef <sabith.th.07@gmail.com>
2018-10-19 20:57:04 +0530

    added products query to fetch and display all products in products page

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -1,9 +1,47 @@
+import gql from 'graphql-tag';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Query } from 'react-apollo';
+import {
+  Button, FlatList, Text, View,
+} from 'react-native';
+
+const productsQuery = gql`
+  query {
+    products {
+      id
+      name
+      price
+      pictureUrl
+    }
+  }
+`;
 
 export default ({ history }) => (
-  <View>
-    <Text style={{ marginLeft: 50, marginTop: 50 }}>Products</Text>
-    <Button title="Create new product" onPress={() => history.push('/new-product')} />
-  </View>
+  <Query query={productsQuery}>
+    {({ loading, error, data: { products } }) => {
+      if (loading) return 'Loading...';
+      if (error) return `Error! ${error.message}`;
+      return (
+        <View>
+          <Text style={{ marginLeft: 50, marginTop: 50 }}>Products</Text>
+          <FlatList
+            data={products.map(p => ({ ...p, key: p.id }))}
+            renderItem={({ item }) => (
+              <View>
+                <Text>
+Item:
+                  {item.name}
+                </Text>
+                <Text>
+Price: $
+                  {item.price}
+                </Text>
+              </View>
+            )}
+          />
+          <Button title="Create new product" onPress={() => history.push('/new-product')} />
+        </View>
+      );
+    }}
+  </Query>
 );
