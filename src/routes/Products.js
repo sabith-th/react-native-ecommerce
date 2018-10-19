8fa0d861b3396cefae1362ae45d9fbdee54a4926
SabithHaneef <sabith.th.07@gmail.com>
2018-10-19 21:56:12 +0530

    fetch and display images from server

--- src/routes/Products.js
+++ src/routes/Products.js
@@ -2,8 +2,38 @@ import gql from 'graphql-tag';
 import React from 'react';
 import { Query } from 'react-apollo';
 import {
-  Button, FlatList, Text, View,
+  Button, FlatList, Image, StyleSheet, Text, View,
 } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 30,
+  },
+  images: {
+    height: 100,
+    width: 100,
+  },
+  row: {
+    display: 'flex',
+    flexDirection: 'row',
+    margin: 10,
+  },
+  right: {
+    flex: 1,
+    marginLeft: 10,
+    marginRight: 30,
+    display: 'flex',
+    alignItems: 'flex-end',
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  price: {
+    fontSize: 20,
+    fontStyle: 'italic',
+  },
+});
 
 const productsQuery = gql`
   query {
@@ -22,20 +52,19 @@ export default ({ history }) => (
       if (loading) return 'Loading...';
       if (error) return `Error! ${error.message}`;
       return (
-        <View>
-          <Text style={{ marginLeft: 50, marginTop: 50 }}>Products</Text>
+        <View style={styles.container}>
           <FlatList
             data={products.map(p => ({ ...p, key: p.id }))}
             renderItem={({ item }) => (
-              <View>
-                <Text>
-Item:
-                  {item.name}
-                </Text>
-                <Text>
-Price: $
-                  {item.price}
-                </Text>
+              <View style={styles.row}>
+                <Image
+                  style={styles.images}
+                  source={{ uri: `http://localhost:4000/${item.pictureUrl}` }}
+                />
+                <View style={styles.right}>
+                  <Text style={styles.name}>{item.name}</Text>
+                  <Text style={styles.price}>{`$${item.price}`}</Text>
+                </View>
               </View>
             )}
           />
