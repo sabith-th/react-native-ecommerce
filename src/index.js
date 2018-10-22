2779f69a0dce025deb2a0665e6fb2fceb02edd4b
SabithHaneef <sabith.th.07@gmail.com>
2018-10-22 22:34:41 +0530

    added subscription setup, update price on click

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloClient } from 'apollo-client';
+import { split } from 'apollo-link';
 import { setContext } from 'apollo-link-context';
+import { WebSocketLink } from 'apollo-link-ws';
 import { createUploadLink } from 'apollo-upload-client';
+import { getMainDefinition } from 'apollo-utilities';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import { AsyncStorage } from 'react-native';
 import { TOKEN_KEY } from './constants';
 import Routes from './routes';
+
+const wsLink = new WebSocketLink({
+  uri: 'ws://localhost:4000/',
+  options: {
+    reconnect: true,
+  },
+});
 
 const authLink = setContext(async (_, { headers }) => {
   const token = await AsyncStorage.getItem(TOKEN_KEY);
@@ -18,8 +28,17 @@ const authLink = setContext(async (_, { headers }) => {
   };
 });
 
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  authLink.concat(createUploadLink({ uri: 'http://localhost:4000' })),
+);
+
 const client = new ApolloClient({
-  link: authLink.concat(createUploadLink({ uri: 'http://localhost:4000' })),
+  link,
   cache: new InMemoryCache(),
 });
 
--- src/routes/Products.js
+++ src/routes/Products.js
@@ -11,6 +11,7 @@ import {
   StyleSheet,
   Text,
   TextInput,
+  TouchableOpacity,
   View,
 } from 'react-native';
 import { TOKEN_KEY } from '../constants';
@@ -89,6 +90,20 @@ export const deleteProductMutation = gql`
   }
 `;
 
+const updatePriceMutation = gql`
+  mutation UpdateProduct($id: ID!, $price: Float) {
+    updateProduct(id: $id, price: $price) {
+      id
+      name
+      price
+      pictureUrl
+      seller {
+        id
+      }
+    }
+  }
+`;
+
 export default class Products extends React.Component {
   state = {
     userId: null,
@@ -107,141 +122,163 @@ export default class Products extends React.Component {
     const { history } = this.props;
     const { userId, query } = this.state;
     return (
-      <Mutation mutation={deleteProductMutation}>
-        {deleteProduct => (
-          <Query query={productsQuery} variables={{ orderBy: 'createdAt_ASC' }} partialRefetch>
-            {({
-              loading, error, data: { productsConnection }, refetch, variables, fetchMore,
-            }) => {
-              if (loading) return <ActivityIndicator size="large" color="#0000ff" />;
-              if (error) return `Error! ${error.message}`;
-              const products = productsConnection.edges;
-              const { hasNextPage } = productsConnection.pageInfo;
-              return (
-                <View style={styles.container}>
-                  <View>
-                    <View style={styles.searchBar}>
-                      <TextInput
-                        name="Search"
-                        placeholder="Search"
-                        value={query}
-                        onChangeText={(text) => {
-                          this.setState({ query: text });
-                          refetch({
-                            where: {
-                              name_contains: text,
-                            },
-                          });
-                        }}
-                      />
-                    </View>
-                    <View style={styles.row}>
-                      <Button
-                        title="Name"
-                        style={styles.sortButton}
-                        onPress={() => refetch({
-                          orderBy: variables.orderBy === 'name_ASC' ? 'name_DESC' : 'name_ASC',
-                          after: null,
-                        })
-                        }
-                      />
-                      <Button
-                        title="Price"
-                        style={styles.sortButton}
-                        onPress={() => refetch({
-                          orderBy: variables.orderBy === 'price_ASC' ? 'price_DESC' : 'price_ASC',
-                          after: null,
-                        })
-                        }
-                      />
-                    </View>
-                  </View>
-                  <Button
-                    title="Create new product"
-                    onPress={() => history.push({ pathname: '/new-product', state: variables })}
-                  />
-                  <FlatList
-                    keyExtractor={item => item.id}
-                    ListFooterComponent={() => hasNextPage && <ActivityIndicator size="large" color="#0000ff" />
-                    }
-                    onEndReached={() => {
-                      if (!loading && hasNextPage) {
-                        fetchMore({
-                          variables: {
-                            after: productsConnection.pageInfo.endCursor,
-                          },
-                          updateQuery: (prev, { fetchMoreResult }) => {
-                            if (!fetchMoreResult) return prev;
-                            return {
-                              productsConnection: {
-                                __typename: 'ProductConnection',
-                                pageInfo: fetchMoreResult.productsConnection.pageInfo,
-                                edges: [
-                                  ...prev.productsConnection.edges,
-                                  ...fetchMoreResult.productsConnection.edges,
-                                ],
-                              },
-                            };
-                          },
-                        });
-                      }
-                    }}
-                    onEndReachedThreshold={0}
-                    data={products.map(p => ({
-                      ...p.node,
-                      showButtons: userId === p.node.seller.id,
-                    }))}
-                    renderItem={({ item }) => (
-                      <View style={styles.row}>
-                        <Image
-                          style={styles.images}
-                          source={{ uri: `http://localhost:4000/${item.pictureUrl}` }}
-                        />
-                        <View style={styles.right}>
-                          <Text style={styles.name}>{item.name}</Text>
-                          <Text style={styles.price}>{`$${item.price}`}</Text>
-                          {item.showButtons && (
-                            <View style={styles.editSection}>
-                              <Button
-                                title="Edit"
-                                onPress={() => history.push({
-                                  pathname: '/edit-product',
-                                  state: {
-                                    item,
-                                    variables,
-                                  },
-                                })
-                                }
-                              />
-                              <Button
-                                title="Delete"
-                                onPress={() => {
-                                  deleteProduct({
-                                    variables: { id: item.id },
-                                    update: (store) => {
-                                      const data = store.readQuery({
-                                        query: productsQuery,
-                                        variables,
-                                      });
-                                      const edges = data.productsConnection.edges.filter(
-                                        x => x.node.id !== item.id,
-                                      );
-                                      data.productsConnection.edges = edges;
-                                      store.writeQuery({ query: productsQuery, data, variables });
-                                    },
-                                  });
-                                }}
-                              />
-                            </View>
-                          )}
+      <Mutation mutation={updatePriceMutation}>
+        {updatePrice => (
+          <Mutation mutation={deleteProductMutation}>
+            {deleteProduct => (
+              <Query query={productsQuery} variables={{ orderBy: 'createdAt_ASC' }} partialRefetch>
+                {({
+                  loading,
+                  error,
+                  data: { productsConnection },
+                  refetch,
+                  variables,
+                  fetchMore,
+                }) => {
+                  if (loading) return <ActivityIndicator size="large" color="#0000ff" />;
+                  if (error) return `Error! ${error.message}`;
+                  const products = productsConnection.edges;
+                  const { hasNextPage } = productsConnection.pageInfo;
+                  return (
+                    <View style={styles.container}>
+                      <View>
+                        <View style={styles.searchBar}>
+                          <TextInput
+                            name="Search"
+                            placeholder="Search"
+                            value={query}
+                            onChangeText={(text) => {
+                              this.setState({ query: text });
+                              refetch({
+                                where: {
+                                  name_contains: text,
+                                },
+                              });
+                            }}
+                          />
+                        </View>
+                        <View style={styles.row}>
+                          <Button
+                            title="Name"
+                            style={styles.sortButton}
+                            onPress={() => refetch({
+                              orderBy:
+                                  variables.orderBy === 'name_ASC' ? 'name_DESC' : 'name_ASC',
+                              after: null,
+                            })
+                            }
+                          />
+                          <Button
+                            title="Price"
+                            style={styles.sortButton}
+                            onPress={() => refetch({
+                              orderBy:
+                                  variables.orderBy === 'price_ASC' ? 'price_DESC' : 'price_ASC',
+                              after: null,
+                            })
+                            }
+                          />
                         </View>
                       </View>
-                    )}
-                  />
-                </View>
-              );
-            }}
-          </Query>
+                      <Button
+                        title="Create new product"
+                        onPress={() => history.push({ pathname: '/new-product', state: variables })}
+                      />
+                      <FlatList
+                        keyExtractor={item => item.id}
+                        ListFooterComponent={() => hasNextPage && <ActivityIndicator size="large" color="#0000ff" />
+                        }
+                        onEndReached={() => {
+                          if (!loading && hasNextPage) {
+                            fetchMore({
+                              variables: {
+                                after: productsConnection.pageInfo.endCursor,
+                              },
+                              updateQuery: (prev, { fetchMoreResult }) => {
+                                if (!fetchMoreResult) return prev;
+                                return {
+                                  productsConnection: {
+                                    __typename: 'ProductConnection',
+                                    pageInfo: fetchMoreResult.productsConnection.pageInfo,
+                                    edges: [
+                                      ...prev.productsConnection.edges,
+                                      ...fetchMoreResult.productsConnection.edges,
+                                    ],
+                                  },
+                                };
+                              },
+                            });
+                          }
+                        }}
+                        onEndReachedThreshold={0}
+                        data={products.map(p => ({
+                          ...p.node,
+                          showButtons: userId === p.node.seller.id,
+                        }))}
+                        renderItem={({ item }) => (
+                          <View style={styles.row}>
+                            <Image
+                              style={styles.images}
+                              source={{ uri: `http://localhost:4000/${item.pictureUrl}` }}
+                            />
+                            <View style={styles.right}>
+                              <Text style={styles.name}>{item.name}</Text>
+                              <TouchableOpacity
+                                onPress={() => updatePrice({
+                                  variables: { id: item.id, price: item.price + 5 },
+                                })
+                                }
+                              >
+                                <Text style={styles.price}>{`$${item.price}`}</Text>
+                              </TouchableOpacity>
+                              {item.showButtons && (
+                                <View style={styles.editSection}>
+                                  <Button
+                                    title="Edit"
+                                    onPress={() => history.push({
+                                      pathname: '/edit-product',
+                                      state: {
+                                        item,
+                                        variables,
+                                      },
+                                    })
+                                    }
+                                  />
+                                  <Button
+                                    title="Delete"
+                                    onPress={() => {
+                                      deleteProduct({
+                                        variables: { id: item.id },
+                                        update: (store) => {
+                                          const data = store.readQuery({
+                                            query: productsQuery,
+                                            variables,
+                                          });
+                                          const edges = data.productsConnection.edges.filter(
+                                            x => x.node.id !== item.id,
+                                          );
+                                          data.productsConnection.edges = edges;
+                                          store.writeQuery({
+                                            query: productsQuery,
+                                            data,
+                                            variables,
+                                          });
+                                        },
+                                      });
+                                    }}
+                                  />
+                                </View>
+                              )}
+                            </View>
+                          </View>
+                        )}
+                      />
+                    </View>
+                  );
+                }}
+              </Query>
+            )}
+          </Mutation>
         )}
       </Mutation>
     );
